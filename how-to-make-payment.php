a025064c57a9ea2fbbdb52b177b20e6a0a89640d
Joshua <joshuanweze270@gmail.com>
2022-03-26 16:57:07 +0100

    How to make payment

---
+++ how-to-make-payment.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" href="/images/logo.png">
+    <title>How to make payment - Royal Network</title>
+</head>
+<body>
+    
+         <a href="https://wa.me/2347033499876?text=Hi, my name is..." class="float" target="_blank">
+          <i class="bi bi-whatsapp my-float"></i>
+          </a>
+
+    <div class="container">
+        <div class="privacy pt-4 col-sm-12 col-md-10 col-lg-8">
+            <div class="text-center ">
+                <h2>Royal Network</h2>
+            </div>
+            <div class="p-4">
+                <div class="mb-2">
+                    <span class="privacy-subhead">HOW TO SUCCESSFULLY MAKE PAYMENT</span>  <span class="line"></span>
+                </div>
+                <div class="text-muted">
+                1. Download the Opay mobile app from your phones Play Store
+                <br>
+
+                2. Fund your Opay account 
+                <br>
+
+                3. Transfer to our Agency's official Opay number <em><strong>+234 703 349 9876</strong></em>
+                <br>
+
+                4. Send our Customer Care a proof of payment via Whatsapp, click the Whatsapp icon at the bottom right of the screen 
+                <br>
+
+                5. Wait for a confirmation of your payment
+                <br>
+                </div>
+            </div>
+
+            <div class="p-4">
+                <div class="mb-2">
+                    <span class="privacy-subhead">Or</span> 
+                </div>
+                <div class="text-muted">
+                1. Visit a nearby Opay POS Stand 
+                <br>
+
+                2. Deposit the required amount via the Opay POS Agent to our Official Opay Number <em><strong>+234 703 349 9876</strong></em>
+                <br>
+
+                3. Send a proof of payment to our customer care via Whatsapp, click the Whatsapp icon at the bottom right of the screen
+                <br>
+
+                4. Wait for a confirmation of your payment 
+                <br>
+                <br>
+
+                <strong>All payments made should be confirmed within 24 hours.</strong>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- footer -->
+
+        <footer class="text-center text-lg-start bg-light container-fluid">
+  
+            <section class="">
+              <div class="container text-center text-md-start mt-5 pt-4">
+               
+                <div class="row mt-3">
+                  
+                  <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                 
+                    <h6 class="text-uppercase fw-bold mb-4 d-flex justify-content-center">
+                      Royal Network
+                    </h6>
+                    <p class="d-flex justify-content-center">
+                    Our Earn Daily platform gives you the opportunity to perform simple tasks while you earn from the comfort of your home.
+                    </p>
+                  </div>
+               
+          
+                  <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                    
+                    <h6 class="text-uppercase fw-bold mb-4 d-flex justify-content-center">
+                      Useful links
+                    </h6>
+                    <p class="d-flex justify-content-center">
+                      <a href="register.php" class="text-reset footer-link">Sign Up</a>
+                    </p>
+                    <p class="d-flex justify-content-center">
+                      <a href="login.php" class="text-reset footer-link">Sign In</a>
+                    </p>
+                  </div>
+          
+          
+                  <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4 ">
+                   
+                    <h6 class="text-uppercase fw-bold mb-4 d-flex justify-content-center">
+                      Contact out customer care
+                    </h6>
+                    <!-- company's email -->
+                    <p class="d-flex justify-content-center">
+                      <i class="fas fa-envelope me-3"></i>
+                      website@email
+                    </p>
+                    <p class="d-flex justify-content-center"><i class="bi bi-telephone"></i> +234 703 349 9876</p>
+                    <p class="d-flex justify-content-center"><i class="bi bi-whatsapp"></i> +234 703 349 9876</p>
+                    
+                  </div>
+                 
+                </div>
+               
+              </div>
+            </section>
+           
+          
+            <!-- Copyright -->
+            <div class="text-center" style="background-color: rgba(0, 0, 0, 0.05);">
+                <div class="text-center mt-5" id="cpright">© <span id="copyrightYear">2022</span> Royal Net </div>
+            </div>
+          </footer>
+    
+
+    <script>
+        let copyrightYear = document.querySelector('#copyrightYear')
+        let Y = new Date().getFullYear()
+      
+        copyrightYear.innerHTML = Y;
+      </script>
+</body>
+</html>
